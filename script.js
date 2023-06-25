6822397a1040ecb8de64514ee0f05fc47bbd036e
Nineikro <nikroesc@gmail.com>
2023-06-25 17:23:00 -0300

    'changes'

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-async function populate() {
+//gets JSON's data
+async function populate() { 
 
-    const requestURL = 'https://raw.githubusercontent.com/Nineikro/testing-json/main/kanji.json?token=GHSAT0AAAAAACDJPAEE53P57YP76SIIVZD2ZEYRR6Q';
+    const requestURL = 'https://raw.githubusercontent.com/Nineikro/testing-json/main/kanji.json?token=GHSAT0AAAAAACDJPAEFMCLUI4MPLTDHCX22ZEYSTXQ'; 'https://raw.githubusercontent.com/Nineikro/testing-json/main/vocabulary.json?token=GHSAT0AAAAAACDJPAEEK3JO7WUWEA2ODADOZEYSNXA';
     const request = new Request(requestURL);
 
     const response = await fetch(request);
@@ -10,51 +11,47 @@ async function populate() {
     populateHeader(superHeroes);
     populateHeroes(superHeroes);
 
-  }
+}
 
-  function populateHeader(obj) {
-    const header = document.querySelector('header');
-    const myH1 = document.createElement('h1');
-    myH1.textContent = obj.songName;
-    header.appendChild(myH1);
+//header stuff
+function populateHeader(obj) {
+  const header = document.querySelector('header');
+  const myH1 = document.createElement('h1');
+  myH1.textContent = obj.songName;
+  header.appendChild(myH1);
 
-    const myPara = document.createElement('p');
-    myPara.textContent = `Creator: ${obj.creator} // Formed: ${obj.formed}`;
-    header.appendChild(myPara);
-  }
+  const myPara = document.createElement('p');
+  myPara.textContent = `Creator: ${obj.creator} // Formed: ${obj.formed}`;
+  header.appendChild(myPara);
+}
 
-  function populateHeroes(obj) {
-    const section = document.querySelector('section');
-    const blocks = obj.parts;
+//song stuff
+function populateHeroes(obj) {
+  const section = document.querySelector('section'); //selects section
+  const blocks = obj.parts; //JSON song parts stored on blocks
 
-    for (const block of blocks) {
-      const myArticle = document.createElement('article');
-      const myH2 = document.createElement('h2');
-      const myPara1 = document.createElement('p');
-      const myPara2 = document.createElement('p');
-      const myPara3 = document.createElement('p');
-      const myList = document.createElement('p');
+  for (const block of blocks) { //loops through all the parts; meaning: shows all the parts till theres no more
+    const myArticle = document.createElement('article'); //creates article element and stores in myArticle
+    const Para1 = document.createElement('p'); //creates paragraph element and stores in para1
 
-      myH2.textContent = block.name;
-      myPara1.textContent = `Secret identity: ${block.secretIdentity}`;
-      myPara2.textContent = `${block.content}`;
-      myPara3.textContent = 'Superpowers:';
+    const songLines = block.powers; //JSON lyrics stores in songLines
+    for (const line of songLines) { //loops through all lines of part
+      const content = document.createElement('p'); //creates paragraph and stores in content
+      content.textContent = line; //all the text in the paragraph are the lines from songLines looped through
+      Para1.appendChild(content); //adds content to para1
 
-      const superPowers = block.powers;
-      for (const power of superPowers) {
-        const listItem = document.createElement('p');
-        listItem.textContent = power;
-        myList.appendChild(listItem);
+      const kanjis = obj.kanji;
+      for (const kanji of kanjis) {
+        if (line.includes(kanjis)) {
+          console.log("hello world")
+        }
       }
-
-      myArticle.appendChild(myH2);
-      myArticle.appendChild(myPara1);
-      myArticle.appendChild(myPara2);
-      myArticle.appendChild(myPara3);
-      myArticle.appendChild(myList);
-
-      section.appendChild(myArticle);
     }
+
+    myArticle.appendChild(Para1); //appends para1 to myarticle
+
+    section.appendChild(myArticle); //appends myarticle to section
   }
+}
 
   populate();
