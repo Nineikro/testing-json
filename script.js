f162c990bd5527349d4f2d23b4351d0073595465
Nineikro <nikroesc@gmail.com>
2023-06-25 22:19:21 -0300

    'changes'

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //gets JSON's data
 async function populate() { 
 
-    const requestURL = 'https://raw.githubusercontent.com/Nineikro/testing-json/main/kanji.json?token=GHSAT0AAAAAACDJPAEFMCLUI4MPLTDHCX22ZEYSTXQ'; 'https://raw.githubusercontent.com/Nineikro/testing-json/main/vocabulary.json?token=GHSAT0AAAAAACDJPAEEK3JO7WUWEA2ODADOZEYSNXA';
+    const requestURL = 'https://raw.githubusercontent.com/Nineikro/testing-json/main/kanji.json?token=GHSAT0AAAAAACDJPAEFFMBA34B7XEXHZ22UZEY4GAA';
     const request = new Request(requestURL);
 
     const response = await fetch(request);
@@ -29,23 +29,24 @@ function populateHeader(obj) {
 function populateHeroes(obj) {
   const section = document.querySelector('section'); //selects section
   const blocks = obj.parts; //JSON song parts stored on blocks
+  const kanjis = obj.kanji;
 
   for (const block of blocks) { //loops through all the parts; meaning: shows all the parts till theres no more
     const myArticle = document.createElement('article'); //creates article element and stores in myArticle
     const Para1 = document.createElement('p'); //creates paragraph element and stores in para1
 
-    const songLines = block.powers; //JSON lyrics stores in songLines
+    const songLines = block.powers; //JSON lyrics stored in songLines
     for (const line of songLines) { //loops through all lines of part
       const content = document.createElement('p'); //creates paragraph and stores in content
       content.textContent = line; //all the text in the paragraph are the lines from songLines looped through
-      Para1.appendChild(content); //adds content to para1
 
-      const kanjis = obj.kanji;
       for (const kanji of kanjis) {
         if (line.includes(kanjis)) {
-          console.log("hello world")
+          console.log(kanji)
         }
       }
+
+      Para1.appendChild(content); //adds content to para1
     }
 
     myArticle.appendChild(Para1); //appends para1 to myarticle
